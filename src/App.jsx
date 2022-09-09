2d0cb7ef0da67290ea80a5b992741d880c5b370c
TheBigH50 <HFangmeier@outlook.com>
2022-09-09 13:41:50 -0500

    set up template ready for inplimenting fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './global.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ constructor(props) {
+  super(props);
+
+  this.state = {
+    user: {
+      username: "Harry",
+      name: "Harrison",
+      rank: "7 kyu",
+      score: "83",
+    }
+  }
+ }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-Header">
+          <h1>Hello World!</h1>
+          {/*Display name, username, rank, score */}
+          <div>
+            <h2>{this.state.user.username}</h2>
+            <p>{this.state.user.name}</p>
+            <p>{this.state.user.rank}</p>
+            <p>{this.state.user.score}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
