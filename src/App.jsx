29cfe34c1910269d9b63f1fa271013899182f7c5
TheBigH50 <HFangmeier@outlook.com>
2022-09-09 13:50:58 -0500

    make fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
 import { Component } from "react";
 
 class App extends Component {
- constructor(props) {
-  super(props);
+  constructor(props) {
+    super(props);
 
-  this.state = {
-    user: {
-      username: "Harry",
-      name: "Harrison",
-      rank: "7 kyu",
-      score: "83",
-    }
+    this.state = {
+      user: {},
+      isLoading: true,
+    };
   }
- }
+
+  getUser() {
+    //fetching coderwars user data
+    fetch("https://www.codewars.com/api/v1/users/TheBigH50")
+      .then((response) => response.json())
+      .then((user) => {
+        this.setState({ user, isLoading: false });
+      })
+      .catch((error) => console.error(error));
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-Header">
-          <h1>Hello World!</h1>
-          {/*Display name, username, rank, score */}
-          <div>
-            <h2>{this.state.user.username}</h2>
-            <p>{this.state.user.name}</p>
-            <p>{this.state.user.rank}</p>
-            <p>{this.state.user.score}</p>
+    if (this.state.isLoading) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <div className="App">
+          <div className="App-Header">
+            <h1>Hello World!</h1>
+            {/*Display name, username, rank, score */}
+            <div>
+              <h2>{this.state.user.username}</h2>
+              <p>{this.state.user.name}</p>
+              <p>{this.state.user.rank}</p>
+              <p>{this.state.user.score}</p>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
